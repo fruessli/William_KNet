830b53706dee3e00e1434eb63de8c7fa81e52871
Xiaoyong Ni <xiaoni@student.ethz.ch>
2021-04-21 23:32:52 +0200

    ERTSNet

--- Simulations/NCLT/model.py
+++ Simulations/NCLT/model.py
@@ -5,6 +5,13 @@ from torch import autograd
 
 from parameters import F_design, H_design
 
+if torch.cuda.is_available():
+    cuda0 = torch.device("cuda:0")  # you can continue going on here, like cuda:1 cuda:2....etc.
+    torch.set_default_tensor_type('torch.cuda.FloatTensor')
+else:
+   cuda0 = torch.device("cpu")
+   print("Running on the CPU")
+
 def f(x):
     return torch.matmul(F_design, x)
 
