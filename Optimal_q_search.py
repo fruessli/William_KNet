2df9b6ac92e03f41ea9b6e6a5f46f889c0bde063
XiaoyongNI <xiaoyongn@126.com>
2021-07-03 20:16:15 +0200

    KNet obs mismatch

--- Optimal_q_search.py
+++ Optimal_q_search.py
@@ -21,7 +21,7 @@ from filing_paths import path_model, path_session
 import sys
 sys.path.insert(1, path_model)
 from parameters import T, T_test, m1x_0, m2x_0, lambda_q_mod, lambda_r_mod, m, n,delta_t_gen,delta_t
-from model import f, h, fInacc
+from model import f, h, fInacc,hInacc
 
 if torch.cuda.is_available():
     device = torch.device("cuda:0")  # you can continue going on here, like cuda:1 cuda:2....etc.
@@ -32,23 +32,23 @@ else:
    print("Running on the CPU")
 
 
-print("Start Data Gen")
-offset = 0
-r2 = torch.tensor([10])
-r = torch.sqrt(r2)
-q_gen = 0
-DatafolderName = 'Simulations/Lorenz_Atractor/data' + '/'
-data_gen = 'data_gen.pt'
-data_gen_file = torch.load(DatafolderName+data_gen, map_location=device)
-[true_sequence] = data_gen_file['All Data']
-[test_target, test_input] = Decimate_and_perturbate_Data(true_sequence, delta_t_gen, delta_t, N_T, h, r, offset)
+# print("Start Data Gen")
+# offset = 0
+# r2 = torch.tensor([10])
+# r = torch.sqrt(r2)
+# q_gen = 0
+# DatafolderName = 'Simulations/Lorenz_Atractor/data' + '/'
+# data_gen = 'data_gen.pt'
+# data_gen_file = torch.load(DatafolderName+data_gen, map_location=device)
+# [true_sequence] = data_gen_file['All Data']
+# [test_target, test_input] = Decimate_and_perturbate_Data(true_sequence, delta_t_gen, delta_t, N_T, h, r, offset)
 
 # vdB = -20 # ratio v=q2/r2
 # v = 10**(vdB/10)
 
 # q2_gen = torch.mul(v,r2)
 # q_gen = torch.sqrt(q2_gen)
-print("data obs noise 1/r2 [dB]: ", 10 * torch.log10(1/r**2))
+# print("data obs noise 1/r2 [dB]: ", 10 * torch.log10(1/r**2))
 # print("data process noise 1/q2 [dB]: ", 10 * torch.log10(1/q_gen**2))
 # #Model
 # sys_model = SystemModel(f, q_gen, h, r, T, T_test, m, n,"Lor")
@@ -57,7 +57,7 @@ print("data obs noise 1/r2 [dB]: ", 10 * torch.log10(1/r**2))
 # DataGen(sys_model, DatafolderName + dataFileName, T, T_test)
 # print("Data Load")
 # [train_input, train_target, cv_input, cv_target, test_input, test_target] = DataLoader_GPU(DatafolderName + dataFileName)  
-print(test_target.size())  
+# print(test_target.size())  
 
 # dataFileName_long = 'data_pen_highresol_q1e-5_long.pt'
 # true_sequence = torch.load(dataFolderName + dataFileName_long, map_location=device)
@@ -73,24 +73,50 @@ print(test_target.size())
 # test_target = test_target_zeroinit[:,:,0:T_test]
 # test_input = test_input_zeroinit[:,:,0:T_test]
 
-q2 = torch.tensor([10, 1,0.1])
-q = torch.sqrt(q2)
+print("Data Load")
+r2_gen = torch.tensor([1])
+r_gen = torch.sqrt(r2_gen)
+rindex = 0
+vdB = -20 # ratio v=q2/r2
+v = 10**(vdB/10)
+q2_gen = torch.mul(v,r2_gen)
+q_gen = torch.sqrt(q2_gen)
+DatafolderName = 'Simulations/Lorenz_Atractor/data' + '/'
+dataFileName = ['data_lor_v20_r1_T2000.pt','data_lor_v20_r1e-1_T2000.pt','data_lor_v20_r1e-2_T2000.pt']
+[train_input_long, train_target_long, cv_input_long, cv_target_long, test_input, test_target] = DataLoader_GPU(DatafolderName + dataFileName[rindex])  
+print("testset:",test_target.size())
+r2 = torch.tensor([10, 1,0.1])
+r = torch.sqrt(r2)
+print("data obs noise 1/r2 [dB]: ", 10 * torch.log10(1/r_gen**2))
+print("data process noise 1/q2 [dB]: ", 10 * torch.log10(1/q_gen**2))
 # dataFileName = ['data_pen_r1_1.pt','data_pen_r1_2.pt','data_pen_r1_3.pt','data_pen_r1_4.pt','data_pen_r1_5.pt']
-for index in range(0, len(q)):
+for index in range(0, len(r)):
 
    #Model
-   sys_model = SystemModel(f, q_gen, h, r, T, T_test, m, n,"Lor")
+   sys_model = SystemModel(f, q_gen, h, r_gen, T, T_test, m, n,"Lor")
    sys_model.InitSequence(m1x_0, m2x_0)
 
-   sys_model_partial = SystemModel(fInacc, q_gen, h, r, T, T_test, m, n,'lor')
-   sys_model_partial.InitSequence(m1x_0, m2x_0)
+   # sys_model_partialf = SystemModel(fInacc, q_gen, h, r_gen, T, T_test, m, n,'lor')
+   # sys_model_partialf.InitSequence(m1x_0, m2x_0)
 
-   sys_model_partial_opt = SystemModel(fInacc, q[index], h, r, T, T_test, m, n,'lor')
-   sys_model_partial_opt.InitSequence(m1x_0, m2x_0)
+   # sys_model_partialf_optq = SystemModel(fInacc, q[index], h, r_gen, T, T_test, m, n,'lor')
+   # sys_model_partialf_optq.InitSequence(m1x_0, m2x_0)
 
-   #Evaluate KF and RTS
-   print("search 1/q2 [dB]: ", 10 * torch.log10(1/q[index]**2))
+   sys_model_partialh = SystemModel(f, q_gen, hInacc, r_gen, T, T_test, m, n,'lor')
+   sys_model_partialh.InitSequence(m1x_0, m2x_0)
+
+   sys_model_partialh_optr = SystemModel(fInacc, q_gen, hInacc, r[index], T, T_test, m, n,'lor')
+   sys_model_partialh_optr.InitSequence(m1x_0, m2x_0)
+
+   #Evaluate EKF True
    [MSE_EKF_linear_arr, MSE_EKF_linear_avg, MSE_EKF_dB_avg, EKF_KG_array, EKF_out] = EKFTest(sys_model, test_input, test_target)
-   [MSE_EKF_linear_arr_partial, MSE_EKF_linear_avg_partial, MSE_EKF_dB_avg_partial, EKF_KG_array_partial, EKF_out_partial] = EKFTest(sys_model_partial, test_input, test_target)
-   [MSE_EKF_linear_arr_partial, MSE_EKF_linear_avg_partial, MSE_EKF_dB_avg_partial, EKF_KG_array_partial, EKF_out_partial] = EKFTest(sys_model_partial_opt, test_input, test_target)
-   # [MSE_ERTS_linear_arr, MSE_ERTS_linear_avg, MSE_ERTS_dB_avg, ERTS_out] = S_Test(sys_model, test_input, test_target)
+   
+   # Search EKF process model mismatch
+   # print("search 1/q2 [dB]: ", 10 * torch.log10(1/q[index]**2))
+   # [MSE_EKF_linear_arr_partial, MSE_EKF_linear_avg_partial, MSE_EKF_dB_avg_partial, EKF_KG_array_partial, EKF_out_partial] = EKFTest(sys_model_partialf, test_input, test_target)
+   # [MSE_EKF_linear_arr_partial, MSE_EKF_linear_avg_partial, MSE_EKF_dB_avg_partial, EKF_KG_array_partial, EKF_out_partial] = EKFTest(sys_model_partialf_optq, test_input, test_target)
+   # Search EKF observation model mismatch
+   print("search 1/r2 [dB]: ", 10 * torch.log10(1/r[index]**2))
+   [MSE_EKF_linear_arr_partial, MSE_EKF_linear_avg_partial, MSE_EKF_dB_avg_partial, EKF_KG_array_partial, EKF_out_partial] = EKFTest(sys_model_partialh, test_input, test_target)
+   [MSE_EKF_linear_arr_partial, MSE_EKF_linear_avg_partial, MSE_EKF_dB_avg_partial, EKF_KG_array_partial, EKF_out_partial] = EKFTest(sys_model_partialh_optr, test_input, test_target)
+  
